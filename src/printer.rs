f4f05981c5f308f9cff780aff3752f67f5d36440
omartarek198 <omar.tarek29@msa.edu.eg>
2024-08-17 21:08:45 +0300

    add basic strings

--- src/printer.rs
+++ src/printer.rs
@@ -16,6 +16,18 @@ pub fn datatypes()
     let y:i32 = -1;
 
     let z:char = 'a';
+}
+
+pub fn strings()
+{
+    let mut constant_mutable_string = "Hi";
+
+    // //error
+    // constant_immutable_string= "bye";
+
+    let mut constant_immutable_string = String::from("hi");
+
+    constant_immutable_string.push('!')
 
     
 }
